8f5f329634910bf7142bfa98ebac0ebc25523ff7
duffleit <davidleitner@live.at>
2015-12-24 00:18:18 +0100

    added ignored TestResult.cs and TestCaseGroupResult.cs File

---
+++ TestExecutor.Common/TestResultEntities/TestCaseGroupResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Remoting;
+
+namespace Testrunner.Common
+{
+
+    public class TestCaseGroupResult
+    {
+        public string TestGroupName { get; private set; }
+        public IReadOnlyList<string> Errors { private set; get; }
+        public int TestCasesCount { get; private set; }
+        public int TestCaseFailureCount { get; private set; }
+        public int TestCaseSuccessCount { get { return TestCasesCount - TestCaseFailureCount; } }
+        
+        public TestCaseGroupResult(string testGroupName)
+        {
+            TestGroupName = testGroupName;
+            Errors = new List<string>();
+        }
+
+        public void AddError(string message)
+        {
+            var errorList = new List<string>(Errors) {message};
+            Errors = new ReadOnlyCollection<string>(errorList);
+            TestCaseFailureCount++;
+        }
+
+        public void IncrementTestCaseCount()
+        {
+            TestCasesCount++;
+        }
+    }
+}
---
+++ TestExecutor.Common/TestResultEntities/TestResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Testrunner.Common;
+
+namespace TestExecutor.Nunit
+{
+    public class TestResult
+    {
+        public TestResult()
+        {
+            TestGroupResults = new List<TestCaseGroupResult>();
+        }
+
+        public IReadOnlyCollection<TestCaseGroupResult> TestGroupResults { get; private set; }
+        public int TestCaseCount { get { return TestGroupResults.Sum(tg => tg.TestCasesCount); } }
+        public int TestCaseSuccessCount { get { return TestGroupResults.Sum(tg => tg.TestCaseSuccessCount); } }
+        public int TestCaseFailureCount { get { return TestGroupResults.Sum(tg => tg.TestCaseFailureCount); } }
+
+        public void AddTestCaseGroupResult(TestCaseGroupResult testCaseGroupResult)
+        {
+            TestGroupResults = new List<TestCaseGroupResult>(TestGroupResults) { testCaseGroupResult };
+        }
+    }
+}
