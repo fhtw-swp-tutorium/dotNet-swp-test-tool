0ca92445101e083b5acd57db466f9aa919dbf720
duffleit <davidleitner@live.at>
2016-02-05 02:39:28 +0100

    upated GlobalAssemblyInfo.cs

--- _solutionItems/GlobalAssemblyInfo.cs
+++ _solutionItems/GlobalAssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 
-//this is the Global Assembly Info.
-//it is located in /_solutionItems/
-
 [assembly: AssemblyCompany("David Leitner")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
