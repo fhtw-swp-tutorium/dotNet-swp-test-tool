1d2bd8f512cb728f5e5d0eaf6f923eac167e28af
David <davidleitner@live.at>
2016-02-08 12:05:12 +0100

    Added Specflow Feature to Observer

---
+++ Tests.Observer/Observer.feature
@@ -0,0 +1,33 @@
+﻿Funktionalität: Observer
+
+Szenario: Es existiert mindest ein Subjekt
+	Gegeben sei Zugriff auf alle verfügbaren Subjekte
+	Dann existiert mindestens ein Subjekt
+
+Szenario: Subjekte haben eine Register Methode
+	Gegeben sei Zugriff auf alle verfügbaren Subjekte
+	Dann existiert mindestens ein Subjekt
+	Dann sollen alle Subjekte eine Reigster Methode haben
+	
+Szenario: Subjekte haben eine Unregister Methode
+	Gegeben sei Zugriff auf alle verfügbaren Subjekte
+	Dann existiert mindestens ein Subjekt
+	Dann sollen alle Subjekte eine Reigster Methode haben
+ 
+Szenario: Subjekte haben eine Update Methode
+	Gegeben sei Zugriff auf alle verfügbaren Subjekte
+	Dann existiert mindestens ein Subjekt
+	Dann sollen alle Subjekte eine Update Methode haben
+
+Szenario: Observer kann sich bei Subjekt registieren
+	Gegeben sei Zugriff auf alle verfügbaren Subjekte
+	Dann existiert mindestens ein Subjekt
+	Dann sollen alle Subjekte eine Reigster Methode haben
+	Dann sollen alle Subjekte eine Update Methode haben
+	Wenn sich bei jedem Subjekt ein Observer mit den Namen "TestObserver" registiert
+	Wenn alle Subjekte die Update Methode Aufrufen
+	Wenn alle Subjekte die Update Methode Aufrufen
+	Dann sollen "TestObserver" "2" mal aufgerufen worden sein
+
+	
+
---
+++ Tests.Observer/ObserverSteps.cs
@@ -0,0 +1,51 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Tests.ExerciseOne
+{
+    [Binding]
+    class ObserverSteps
+    {
+        [Given(@"Zugriff auf alle verfügbaren Subjekte")]
+        public void AngenommenZugriffAufAlleVerfugbarenSubjekte()
+        {
+            ////C:\Work\FH\dotNet-sampleSolution\SampleSolution\SampleSolution\bin\Debug\SampleSolution.exe
+        }
+
+        [Then(@"existiert mindestens ein Subjekt")]
+        public void DannExistiertMindestensEinSubjekt()
+        {
+            ////C:\Work\FH\dotNet-sampleSolution\SampleSolution\SampleSolution\bin\Debug\SampleSolution.exe
+        }
+
+        [Then(@"sollen alle Subjekte eine Reigster Methode haben")]
+        public void DannSollenAlleSubjekteEineReigsterMethodeHaben()
+        {
+            //C:\Work\FH\dotNet-sampleSolution\SampleSolution\SampleSolution\bin\Debug\SampleSolution.exe
+        }
+
+        [Then(@"sollen alle Subjekte eine Update Methode haben")]
+        public void DannSollenAlleSubjekteEineUpdateMethodeHaben()
+        {
+            //C:\Work\FH\dotNet-sampleSolution\SampleSolution\SampleSolution\bin\Debug\SampleSolution.exe
+        }
+
+        [When(@"sich bei jedem Subjekt ein Observer mit den Namen ""(.*)"" registiert")]
+        public void WennSichBeiJedemSubjektEinObserverMitDenNamenRegistiert(string p0)
+        {
+            //C:\Work\FH\dotNet-sampleSolution\SampleSolution\SampleSolution\bin\Debug\SampleSolution.exe
+        }
+
+        [When(@"alle Subjekte die Update Methode Aufrufen")]
+        public void WennAlleSubjekteDieUpdateMethodeAufrufen()
+        {
+            //C:\Work\FH\dotNet-sampleSolution\SampleSolution\SampleSolution\bin\Debug\SampleSolution.exe
+        }
+
+        [Then(@"sollen ""(.*)"" ""(.*)"" mal aufgerufen worden sein")]
+        public void DannSollenMalAufgerufenWordenSein(string p0, int p1)
+        {
+            //C:\Work\FH\dotNet-sampleSolution\SampleSolution\SampleSolution\bin\Debug\SampleSolution.exe
+        }
+
+    }
+}
