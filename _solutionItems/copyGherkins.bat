547028b93a0f205417fdbbe9252bf013f4275724
duffleit <davidleitner@live.at>
2016-02-25 00:00:52 +0100

    updatd links in copyGherkins.bat

--- _solutionItems/copyGherkins.bat
+++ _solutionItems/copyGherkins.bat
@@ -1,3 +1,3 @@
 ::copy Command.feature ..
-copy Gherkins\Observer.feature ..\Tests.Observer\Observer.feature
-copy Gherkins\Singleton.feature ..\Tests.Singleton\Singleton.feature
+copy ..\pattern-specifications\Observer.feature ..\Tests.Observer\Observer.feature
+copy ..\pattern-specifications\Singleton.feature ..\Tests.Singleton\Singleton.feature
