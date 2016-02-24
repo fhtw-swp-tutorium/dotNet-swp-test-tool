7af40f6b1aaf9315a9420476d3b27748e2b02f8c
duffleit <davidleitner@live.at>
2016-02-25 00:17:23 +0100

    added Command tests

---
+++ Tests.Command/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests.Command")]
+[assembly: AssemblyProduct("Tests.Command")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3cc5c073-090b-467e-8e62-a3f4b8b50797")]
+
--- _solutionItems/copyGherkins.bat
+++ _solutionItems/copyGherkins.bat
@@ -1,3 +1,3 @@
-::copy Command.feature ..
 copy ..\pattern-specifications\Observer.feature ..\Tests.Observer\Observer.feature
 copy ..\pattern-specifications\Singleton.feature ..\Tests.Singleton\Singleton.feature
+copy ..\pattern-specifications\Command.feature ..\Tests.Command\Command.feature
